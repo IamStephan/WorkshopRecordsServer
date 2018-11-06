0dfe04dc7d66cae3a42e91dba4c038adbbf81559
Stephan Burger <stephanburger54@gmail.com>
2018-11-06 11:06:38 +0200

    index.js is updated to socket io v2

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const port = process.env.PORT || 3000
+const app = require('http').createServer(handler)
+const io = require('socket.io')(app);
+const fs = require('fs');
 
-var io = require('socket.io')(port);
+app.listen(port);
+
+function handler (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+}
 
 io.on('connection', function (socket) {
   console.log('user connected')
